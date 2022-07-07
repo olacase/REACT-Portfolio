77600231208a7472686bc208d27d7c4b2f4d07bd
olacase@gmail.com <olacase@gmail.com>
2022-07-07 18:14:10 -0500

    Updates

---
+++ precache-manifest.d188370f81031f5bcf675cf7162195c3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e7063729843ece4161369c68d65cf9f",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "cab6aee6916d87403b9c",
+    "url": "/react-portfolio/static/css/2.34e3f391.chunk.css"
+  },
+  {
+    "revision": "27d13841ba9b583495ce",
+    "url": "/react-portfolio/static/css/main.8794fb6c.chunk.css"
+  },
+  {
+    "revision": "cab6aee6916d87403b9c",
+    "url": "/react-portfolio/static/js/2.e06d076e.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/react-portfolio/static/js/2.e06d076e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9ceec4dbc394e8130ca",
+    "url": "/react-portfolio/static/js/3.db9fdbee.chunk.js"
+  },
+  {
+    "revision": "27d13841ba9b583495ce",
+    "url": "/react-portfolio/static/js/main.dcddbc6d.chunk.js"
+  },
+  {
+    "revision": "5fcf1cae0ac1631127a1",
+    "url": "/react-portfolio/static/js/runtime-main.3e6874fc.js"
+  },
+  {
+    "revision": "a54909519c704e0c9a52bc69afb4266b",
+    "url": "/react-portfolio/static/media/ResumeOla.a5490951.pdf"
+  },
+  {
+    "revision": "ed3745e88fe1b300a655a5740511a9a1",
+    "url": "/react-portfolio/static/media/ola.ed3745e8.png"
+  }
+]);
